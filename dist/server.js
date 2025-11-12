b655e3c3ddad69c94d51f0189e13122605db694e
vishurashu <pal.vishal94@gmail.com>
2025-11-12 12:03:28 +0530

    XCFG

--- dist/server.js
+++ dist/server.js
@@ -12,22 +12,24 @@ const dbConnection_1 = require("./config/dbConnection");
 const admin_1 = __importDefault(require("./app/router/admin"));
 const user_1 = __importDefault(require("./app/router/user"));
 const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
-const swaggerFile = require(path_1.default.join(__dirname, '../swagger-output.json')); // ✅ JSON import works if tsconfig is configured
+const swaggerFile = require(path_1.default.join(__dirname, "../swagger-output.json"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 5000;
 app.use((0, cors_1.default)({
-    origin: "*"
+    origin: '*',
+    credentials: true,
 }));
+// ✅ Additional headers (optional but recommended)
 app.use(express_1.default.json());
-app.use(express_1.default.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express_1.default.urlencoded({ extended: true, limit: "50mb" }));
 // ✅ Serve uploads folder for images
 app.use("/uploads", express_1.default.static(path_1.default.join(__dirname, "../uploads")));
-app.use('/admin', admin_1.default);
-app.use('/api', user_1.default);
+app.use("/admin", admin_1.default);
+app.use("/api", user_1.default);
 // ✅ Swagger UI route
-app.use('/api-docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerFile));
-app.get('/', (req, res) => {
-    res.send('Hello from TypeScript Express!');
+app.use("/api-docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerFile));
+app.get("/", (req, res) => {
+    res.send("Hello from TypeScript Express!");
 });
 app.listen(PORT, () => {
     (0, dbConnection_1.connectDB)();
