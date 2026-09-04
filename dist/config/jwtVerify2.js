202bbd7fb0959afa905606571ebe46366c380c65
Sumeet Kumar <sumeet.kumar@dovetailsolutions.in>
2026-09-04 13:08:30 +0530

    Remove dist from git tracking and add to .gitignore

--- dist/config/jwtVerify2.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tokenCheck = void 0;
-const tokenCheck_1 = require("./tokenCheck");
-// User/mobile-side auth: user / manager / sale_person.
-exports.tokenCheck = (0, tokenCheck_1.createTokenCheck)(["user", "manager", "sale_person"]);
